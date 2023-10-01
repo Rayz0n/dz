6f66a9b036619459f7203efec990910a15a58a5e
Rayz0N <Rayz0N>
2023-10-01 15:24:48 +0300

    01.10.23

---
+++ main.py
@@ -0,0 +1,23 @@
+from graph import *
+
+penColor("black")
+penSize(2)
+brushColor("red")
+circle(100, 100, 30)
+brushColor("blue")
+polygon([(130, 100), (280, 100), (205, 50)])
+brushColor("yellow")
+circle(310, 100, 30)
+brushColor("green")
+polygon([(130, 101), (280, 101), (205, 150)])
+brushColor("red")
+polygon([(205, 255), (235, 365), (165, 365)])
+circle(205, 250, 15)
+brushColor("green")
+polygon([(226, 330), (236, 365), (270, 365), (290, 270)])
+circle(290, 270, 15)
+brushColor("blue")
+polygon([(164, 365), (176,330), (110, 270), (130, 365)])
+circle(110, 270, 15)
+
+run()
