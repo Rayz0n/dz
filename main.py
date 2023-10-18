68cf73827639f8d894f5f6192a6d7699c544ac8f
Rayz0N <Rayz0N>
2023-10-18 22:35:41 +0300

    na 19.10.23

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-from graph import *
+# Dz na 19.10.23
 
-penSize(2)
-brushColor("blue")
-rectangle(150, 150, 250, 250)
-polygon([(290, 125), (250, 150), (250, 250), (290, 230)])
-polygon([(290, 125), (250, 150), (150, 150), (190, 125)])
-
-run()
+c=int(input("c"))
+b=int(input("b"))
+d=int(input("d"))
+a=(c+b**5*3-1)/2*d
+print(a)
