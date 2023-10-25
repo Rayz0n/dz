76a19d8ab5a03a8eef2aa726626437f5b3fa4a2b
Rayz0N <Rayz0N>
2023-10-25 19:33:31 +0300

    26.10.23

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
-# Dz na 19.10.23
+# Dz na 26.10.23
 
-c=int(input("c"))
-b=int(input("b"))
-d=int(input("d"))
-a=(c+b**5*3-1)/2*d
-print(a)
+# №2
+abc = int(input("Число"))
+a = inp//100
+b = inp%100//10
+c = inp%100%10
+print(a, b, c)
+
+# №3
+abc = int(input("Число"))
+a = abc // 100 + abc % 100 // 10 + abc % 100 % 10
+b = abc // 100 * 100
+c = abc % 100 % 10 * 10
+d = abc % 100 // 10
+print(a, b+c+d)
