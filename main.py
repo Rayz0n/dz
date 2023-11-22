4a58e6a901e770c9e9ba3b2b658cc0a7d8505879
Rayz0N <Rayz0N>
2023-11-22 20:50:00 +0300

    22.11.23

--- main.py
+++ main.py
@@ -1,35 +1,43 @@
-# Dz na 16.11.23
-import random
+# Dz na 22.11.23
+a = int(input("Антон"))
+b = int(input("Борис"))
+w = int(input("Виктор"))
 
-# №1
-a = random.randint(1, 6)
-print(a)
-
-# №2
-a = random.randint(1, 3)
-b = random.randint(4, 6)
-print(a, b)
-
-# №3
-a = random.randint(1, 90)
-print(a)
-a = random.randint(1, 90)
-print(a)
-a = random.randint(1, 90)
-print(a)
-a = random.randint(1, 90)
-print(a)
-a = random.randint(1, 90)
-print(a)
-
-# №4
-a = random.randint(1, 18)
-print(a)
-a = random.randint(19, 36)
-print(a)
-a = random.randint(37, 54)
-print(a)
-a = random.randint(55, 72)
-print(a)
-a = random.randint(73, 90)
-print(a)
+if a == b == w:
+    print("Все одинакового возраста")
+elif a > b:
+    print(2)
+    if a > w:
+        print("Антон старший")
+    elif a == w:
+        print("Антон и Виктор старшие")
+elif a > w:
+    print(3)
+    if a > b:
+        print("Антон старший")
+    elif a == b:
+        print("Антон и Борис старшие")
+elif b > a:
+    print(4)
+    if b > w:
+        print("Борис старший")
+    elif b == w:
+        print("Борис и Виктор старшие")
+elif b > w:
+    print(5)
+    if b > a:
+        print("Борис старший")
+    elif b == a:
+        print("Борис и Антон старшие")
+elif w > a:
+    print(6)
+    if w > b:
+        print("Виктор старший")
+    elif w == b:
+        print("Борис и Виктор старшие")
+elif w > b:
+    print(7)
+    if w > a:
+        print("Виктор старший")
+    elif w == a:
+        print("Виктор и Антон старшие")
