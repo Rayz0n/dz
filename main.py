1909767c1e86298c5ec9ccbc4c9b8c89d184981a
Rayz0N <Rayz0N>
2023-12-14 13:42:59 +0300

    14.12.23

--- main.py
+++ main.py
@@ -1,19 +1,31 @@
-# Dz na 07.12.23
-a = int(input("Первый талон: "))
-b = int(input("Второй талон: "))
-c = int(input("Третий талон: "))
-d = int(input("Четвёртый талон: "))
-a = a // 1000 + a % 1000 // 100 + a % 1000 % 100 // 10 + a % 1000 % 100 % 10
-b = b // 1000 + b % 1000 // 100 + b % 1000 % 100 // 10 + b % 1000 % 100 % 10
-c = c // 1000 + c % 1000 // 100 + c % 1000 % 100 // 10 + c % 1000 % 100 % 10
-d = d // 1000 + d % 1000 // 100 + d % 1000 % 100 // 10 + d % 1000 % 100 % 10
-count = 0
-if a % 2 == 0:
-    count += 1
-if b % 2 == 0:
-    count += 1
-if c % 2 == 0:
-    count += 1
-if d % 2 == 0:
-    count += 1
-print(count)
+# Dz na 14.12.23
+
+# Квадрат
+from graph import *
+windowSize(1000, 1000)
+x = int(input("x = "))
+y = int(input("y="))
+n = int(input("n = "))
+if y> 1000 or x+n*40> 1000 or x-n*40 > 1000 or x < 0 or y < 0 or x+n*40 < 0 or x-n*40 < 0 or n <= 0:
+    print("недоступные координаты")
+    exit()
+penColor("black") 
+penSize(3)
+brushColor("blue")
+polygon([(x, y), (x+n*40, y+n*40), (x-n*40, y+n*40), (x,y)])
+run()
+
+# Буква Г
+from graph import *
+windowSize(1000, 1000)
+x = int(input("x = "))
+y = int(input("y="))
+n = int(input("n = "))
+if y+n*90 > 1000 or x+n*20 > 1000 or y+n*20 > 1000 or x+n*50 > 1000 or x > 1000 or y> 1000 or x < 0 or y < 0 or n < 0: 
+    print("недоступные координаты")
+    exit()
+penColor("black") 
+penSize(3)
+brushColor("blue")
+polygon([(x, y), (x, y+n*90), (x+n*20, y+n*90), (x+n*20, y+n*20), (x+n*50, y+n*20), (x+n*50, y), (x,y)])
+run()
