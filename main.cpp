030da9a40ed1425f5cfa54848c36cbedb986ef65
Rayz0N <Rayz0N>
2024-03-01 18:53:28 +0300

    04.03

---
+++ main.cpp
@@ -0,0 +1,46 @@
+// Dz na 04.03
+
+// №1
+# include<iostream>
+using namespace std;
+
+int main(){
+    int a, b, c, d;
+    cout << "Введите три числа:" << endl;
+    cin >> a >> b >> c;
+    d = a+b+c;
+    cout << a << "+" << b << "+" << c << "=" << d << endl;
+    return 0;
+}  
+
+// №2
+# include<iostream>
+using namespace std;
+
+int main(){
+    int a, b, c, d;
+    cout << "Введите три числa:" << endl;
+    cin >> a >> b >> c;
+    d = a-b-c;
+    cout << a << "-" << b << "-" << c << "=" << d << endl;
+    d = a*b*c;
+    cout << a << "*" << b << "*" << c << "=" << d << endl;
+    return 0;
+}
+
+// №3
+# include<iostream>
+using namespace std;
+
+int main(){
+    float a, b, c, d;
+    cout << "Введите три числа:" << endl;
+    cin >> a >> b >> c;
+    d = a+b+c;
+    cout << a << "+" << b << "+" << c << "=" << d << endl;
+    d = a*b*c;
+    cout << a << "*" << b << "*" << c << "=" << d << endl;
+    d = (a+b+c)/3;
+    cout << a << "*" << b << "*" << c << "=" << d << endl;
+    return 0;
+}
---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,30 @@
+# include<iostream>
+// using namespace std;
+
+// int main(){
+//     int a, b, c, d;
+//     cout << "Введите три числa:" << endl;
+//     cin >> a >> b >> c;
+//     d = a-b-c;
+//     cout << a << "-" << b << "-" << c << "=" << d << endl;
+//     d = a*b*c;
+//     cout << a << "*" << b << "*" << c << "=" << d << endl;
+//     return 0;
+// }
+
+// // №3
+// # include<iostream>
+// using namespace std;
+
+// int main(){
+//     float a, b, c, d;
+//     cout << "Введите три числа:" << endl;
+//     cin >> a >> b >> c;
+//     d = a+b+c;
+//     cout << a << "+" << b << "+" << c << "=" << d << endl;
+//     d = a*b*c;
+//     cout << a << "*" << b << "*" << c << "=" << d << endl;
+//     d = (a+b+c)/3;
+//     cout << a << "*" << b << "*" << c << "=" << d << endl;
+//     return 0;
+// }
