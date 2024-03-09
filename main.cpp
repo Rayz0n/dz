fa8da40134eb35040336fb57f4d3e915732ba994
Rayz0N <Rayz0N>
2024-03-09 14:22:52 +0300

    11.03

--- main.cpp
+++ main.cpp
@@ -1,15 +1,16 @@
-// Dz na 04.03
+// Dz na 11.03
 
 // №1
 # include<iostream>
 using namespace std;
 
 int main(){
-    int a, b, c, d;
-    cout << "Введите три числа:" << endl;
-    cin >> a >> b >> c;
-    d = a+b+c;
-    cout << a << "+" << b << "+" << c << "=" << d << endl;
+    int a, b, c;
+    cout << "Введите время в секундах: ";
+    cin >> a;
+    b = a / 60;
+    c = a % 60;
+    cout << b << " мин. " << c << " с." << endl;
     return 0;
 }  
 
@@ -19,12 +20,12 @@ using namespace std;
 
 int main(){
     int a, b, c, d;
-    cout << "Введите три числa:" << endl;
-    cin >> a >> b >> c;
-    d = a-b-c;
-    cout << a << "-" << b << "-" << c << "=" << d << endl;
-    d = a*b*c;
-    cout << a << "*" << b << "*" << c << "=" << d << endl;
+    cout << "Введите время в секундах: ";
+    cin >> a;
+    b = a / 3600;
+    c = (a - (b*3600))/60;
+    d = a - (b * 3600 + c * 60);
+    cout << b << " ч. " << c << " мин. " << d << " с. " << endl ;
     return 0;
 }
 
@@ -33,14 +34,11 @@ int main(){
 using namespace std;
 
 int main(){
-    float a, b, c, d;
-    cout << "Введите три числа:" << endl;
-    cin >> a >> b >> c;
-    d = a+b+c;
-    cout << a << "+" << b << "+" << c << "=" << d << endl;
-    d = a*b*c;
-    cout << a << "*" << b << "*" << c << "=" << d << endl;
-    d = (a+b+c)/3;
-    cout << a << "*" << b << "*" << c << "=" << d << endl;
-    return 0;
+    int a, b, c, d;
+    cout << "Введите номер урока: ";
+    cin >> a;
+    d = (8*60+30) + (a * 45 + (a-1) * 10);
+    b = d / 60;
+    c = d - (b * 60);
+    cout << b << "-" << c << endl;
 }
