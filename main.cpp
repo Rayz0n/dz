95b7fd24964b5cc140c509039cb0ce311c1e0ec0
Rayz0N <Rayz0N>
2024-03-24 16:47:30 +0300

    25.03

--- main.cpp
+++ main.cpp
@@ -1,41 +1,72 @@
-// Dz na 11.03
+// Dz na 25.03
 
 // №1
 # include<iostream>
-# include<cmath>
+# include<random>
+# include<ctime>
 using namespace std;
 
 int main(){
-    int a, b, c;
-    cout << "Введите число и степень: ";
-    cin >> a >> b;
-    c = pow(a, b);
-    cout << c << endl;
+    srand(time(0)); 
+    cout << rand() % 99 << endl;
+    cout << rand() % 99 << endl;
+    cout << rand() % 99 << endl;
     return 0;
-}  
+}
 
 // №2
 # include<iostream>
-# include<cmath>
+# include<random>
+# include<ctime>
 using namespace std;
 
 int main(){
-    int a, b;
-    cout << "Введите отрицательное число: ";
-    cin >> a;
-    b = abs(a) + 1;
-    cout << b << endl ;
+    srand(time(0)); 
+    int a = 0;
+    a += rand() % (6 - 1 + 1) + 1;
+    a += rand() % (6 - 1 + 1) + 1;
+    a += rand() % (6 - 1 + 1) + 1;
+    a += rand() % (6 - 1 + 1) + 1;
+    a += rand() % (6 - 1 + 1) + 1;
+    cout << a << endl;
     return 0;
 }
 
 // №3
 # include<iostream>
+# include<random>
+# include<cmath>
+# include<ctime>
 using namespace std;
 
 int main(){
-    int a, b;
-    cout << "Введите объём фотографии в мегабайтах: ";
-    cin >> a;
-    b = (1024*2)/a;
-    cout << "Такая фотография поместится " << b << " раз(а)" << endl;
+    srand(time(0)); 
+    int a, b, c, d;
+    a = rand() % (6 - 1 + 1) + 1;
+    b = rand() % (6 - 1 + 1) + 1;
+    c = rand() % (6 - 1 + 1) + 1;
+    cout << "Выпало очков:" << endl << a << " " << b << " " << c << endl;
+    d = a * 100 + b * 10 + c;
+    cout << "Число: " << d << endl;
+    d = pow(d, 2);
+    cout << "Его квадрат " << d << endl;
+    return 0;
+}
+
+// №4
+# include<iostream>
+# include<random>
+# include<ctime>
+using namespace std;
+
+int main(){
+    srand(time(0)); 
+    int a, b, c, d;
+    a = rand() % (999 - 100 + 1) + 100;
+    cout << "Получено число " << a << endl;
+    b = a / 100;
+    c = a % 100 / 10;
+    d = a % 10 % 10;
+    cout << "сотни: " << b << endl << "десятки: " << c << endl << "едницы: " << d << endl;
+    return 0;
 }
